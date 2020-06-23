10a125dabe1dbc7c7fde4977e2cf313f3cb0ea24
efnushtaev <efnushtaev@gmail.com>
2020-06-23 14:50:39 +0300

    mock questions added

---
+++ src/mocks/questions.js
@@ -0,0 +1,41 @@
+export default [{
+  type: `genre`,
+  genre: `indie`,
+  answers: [
+    {
+      genre: `hard`,
+      src:`https://upload.wikimedia.org/wikipedia/commons/5/53/BWV_543-prelude.ogg`
+    },
+    {
+      genre: `blues`,
+      src:`https://upload.wikimedia.org/wikipedia/commons/5/53/BWV_543-prelude.ogg`
+    },
+    {
+      genre: `indie`,
+      src:`https://upload.wikimedia.org/wikipedia/commons/5/53/BWV_543-prelude.ogg`
+    },
+    {
+      genre: `jazz`,
+      src:`https://upload.wikimedia.org/wikipedia/commons/5/53/BWV_543-prelude.ogg`
+    },
+  ]
+},
+{
+  type: `artist`,
+  artist: `Amy Whinehouse`,
+  src: `https://upload.wikimedia.org/wikipedia/commons/5/53/BWV_543-prelude.ogg`,
+  answers: [
+    {
+      artist: `The Beatles`,
+      picture: `https://smk.org.uk/wp-content/uploads/avatar.jpg`
+    },
+    {
+      artist: `Chuck Berry`,
+      picture: `https://smk.org.uk/wp-content/uploads/avatar.jpg`
+    },
+    {
+      artist: `Jimi Hendrix`,
+      picture: `https://smk.org.uk/wp-content/uploads/avatar.jpg`
+    }]
+  }
+]
